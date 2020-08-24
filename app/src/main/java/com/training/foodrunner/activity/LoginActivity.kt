6a5e254fef28c7ec8cf1c9356197ba88ac07d9a4
Saicharan Pogul <36988048+saicharanpogul@users.noreply.github.com>
2020-08-24 16:08:02 +0530

    Update LoginActivity.kt

--- app/src/main/java/com/training/foodrunner/activity/LoginActivity.kt
+++ app/src/main/java/com/training/foodrunner/activity/LoginActivity.kt
@@ -138,7 +138,7 @@ class LoginActivity : AppCompatActivity() {
                         override fun getHeaders(): MutableMap<String, String> {
                             val headers = HashMap<String, String>()
                             headers["content-type"] = "application/json"
-                            headers["token"] = "0347b8d3845796"
+                            headers["token"] = "###############"
                             return headers
                         }
                     }
