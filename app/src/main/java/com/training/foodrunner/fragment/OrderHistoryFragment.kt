4f64f10016bfb2c52fd765da80ae82f54e0442ac
Saicharan Pogul <36988048+saicharanpogul@users.noreply.github.com>
2020-08-24 16:12:53 +0530

    Update OrderHistoryFragment.kt

--- app/src/main/java/com/training/foodrunner/fragment/OrderHistoryFragment.kt
+++ app/src/main/java/com/training/foodrunner/fragment/OrderHistoryFragment.kt
@@ -122,7 +122,7 @@ class OrderHistoryFragment : Fragment() {
                     override fun getHeaders(): MutableMap<String, String> {
                         val headers = HashMap<String, String>()
                         headers["content-type"] = "application/json"
-                        headers["token"] = "0347b8d3845796"
+                        headers["token"] = "################"
                         return headers
                     }
                 }
