65bc3a8c0511d023c09207aa442cb4e400d2ccb6
Saicharan Pogul <36988048+saicharanpogul@users.noreply.github.com>
2020-08-24 16:09:13 +0530

    Update RestaurantDetails.kt

--- app/src/main/java/com/training/foodrunner/activity/RestaurantDetails.kt
+++ app/src/main/java/com/training/foodrunner/activity/RestaurantDetails.kt
@@ -137,7 +137,7 @@ class RestaurantDetails : AppCompatActivity() {
                     override fun getHeaders(): MutableMap<String, String> {
                         val headers = HashMap<String, String>()
                         headers["content-type"] = "application/json"
-                        headers["token"] = "0347b8d3845796"
+                        headers["token"] = "############"
                         return headers
                     }
                 }
